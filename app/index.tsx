5e0b22fa07feb589434f932fc390c529802b98f1
mustafa-mahmud <mithuweb000@gmail.com>
2026-07-22 21:19:01 +0600

    @convex-dev/auth, @auth/core, expo-secure-store,react-native-webview,snack-sdk,zustand,expo-haptics,expo-symbols,expo-clipboard,react-native-qrcode-svg,react-native-svg installed successfully

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,7 @@ export default function Index() {
   ////////////////////////////////////////
   return (
     <View>
-      <Text className="text-red-500 text-center">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+      <Text className="text-red-500 text-center">React Native</Text>
     </View>
   );
 }
